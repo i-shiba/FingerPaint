eea389659aed2bb1ef9a07b7392720303bc16cb1
ict <ict@R1-07.local>
2012-09-13 22:02:25 +0900

    as commit

---
+++ src/sample/application/fingerpaint/FingerPaintActivity.java
@@ -0,0 +1,28 @@
+package sample.application.fingerpaint;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class FingerPaintActivity extends Activity implements OnTouchListener{
+	
+	public Canvas  canvas;
+	public Paint   panit;
+	public Path    path;
+	public Bitmap  bitmap;
+	public Float   x1;
+	public Float   y1;
+	public Integer w;
+	public Integer h;
+	
+	public boolean onTouch (View v, MotionEvent event){
+		return Boolean.valueOf(true);
+		//return Boolean2.TRUE;
+	}
+
+}
