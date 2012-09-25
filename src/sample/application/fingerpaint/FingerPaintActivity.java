a981c6a5fb89635143127280c90e25002f644c77
i-shiba <sbtior@yahoo.co.jp>
2012-09-25 17:09:51 +0900

    home-P133 commit

--- src/sample/application/fingerpaint/FingerPaintActivity.java
+++ src/sample/application/fingerpaint/FingerPaintActivity.java
@@ -1,14 +1,24 @@
 package sample.application.fingerpaint;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DecimalFormat;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -28,7 +38,6 @@ public class FingerPaintActivity extends Activity implements OnTouchListener{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		ImageView iv = (ImageView)this.findViewById(R.id.imageView1);
 		Display disp = ((WindowManager)this.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
@@ -48,10 +57,32 @@ public class FingerPaintActivity extends Activity implements OnTouchListener{
 		this.canvas.drawColor(Color.WHITE);
 		iv.setImageBitmap(this.bitmap);
 		iv.setOnTouchListener(this);
+		
 	}
 
 
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater mi = getMenuInflater();
+		mi.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+			case R.id.menu_save:
+				save();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+
 	public boolean onTouch (View v, MotionEvent event){
 		float x = event.getX();
 		float y = event.getY();
@@ -87,6 +118,54 @@ public class FingerPaintActivity extends Activity implements OnTouchListener{
 //		return Boolean.valueOf(true);
 		//return Boolean2.TRUE;
 	}
-
+	
+	public void save(){
+		SharedPreferences prefs = this.getSharedPreferences(
+									"FingerPaintPreferences",MODE_PRIVATE);
+		int imageNumber = prefs.getInt("imageNumber", 1);
+		File file = null;
+		
+		if(externalMediaChecker()){
+			DecimalFormat form = new DecimalFormat("0000");
+			String path = Environment.getExternalStorageDirectory() + "/mypaint/";
+			File outDir = new File(path);
+			if(!outDir.exists()){
+				outDir.mkdir();
+			}
+			
+			do{
+				file = new File(path + "img" + form.format(imageNumber) + ".png");
+				imageNumber++;
+			}while(file.exists());
+			if(writeImage(file)){
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.putInt("imageNumber", imageNumber);
+				editor.commit();
+			}
+		}
+	}
+	
+	public Boolean writeImage(File file){
+		try{
+			FileOutputStream fo = new FileOutputStream(file);
+			bitmap.compress(CompressFormat.PNG, 100, fo);
+			fo.flush();
+			fo.close();
+		}catch(Exception e){
+			System.out.println(e.getLocalizedMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	public Boolean externalMediaChecker(){
+		Boolean result = false;
+		String status = Environment.getExternalStorageState();
+		if(status.equals(Environment.MEDIA_MOUNTED)){
+			result = true;
+		}
+		return result;
+	}
+	
 }
 
